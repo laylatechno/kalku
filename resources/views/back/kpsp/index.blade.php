df21e4a1f2b334390c4ea25ea5da233d21761a57
Layla Techno <154958698+laylatechno@users.noreply.github.com>
2025-01-30 07:27:59 +0700

    replace Catering Pro

--- resources/views/back/kpsp/index.blade.php
+++ resources/views/back/kpsp/index.blade.php
@@ -8,7 +8,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Catering Pro - {{ $subtitle }}</h3>
+                <h3 class="card-title">Kalkulating - {{ $subtitle }}</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
